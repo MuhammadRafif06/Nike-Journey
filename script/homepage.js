11d3efa8f116640d8b17fdefcd0c6f1ddee386d4
Muhammad Rafif <radif.adityo6@gmail.com>
2025-03-16 21:27:44 +0700

    first commit

---
+++ script/homepage.js
@@ -0,0 +1,11 @@
+let currentIndex = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelector(".slides");
+    const totalSlides = document.querySelectorAll(".slide").length;
+    currentIndex = (currentIndex + step + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+// auto slide setiap 3 detik
+setInterval(() => moveSlide(1), 3000);
